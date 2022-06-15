78b3d2558f442b5d36efa7981d51097d104bb2a6
richard gichuki <chadgichuki@gmail.com>
2022-06-15 14:33:06 +0300

    Like button works, errors peoperly displaying

--- main.js
+++ main.js
@@ -4,7 +4,29 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const likeButtons= document.querySelectorAll('span.like-glyph')
 
+likeButtons.forEach(likeButton => {
+    likeButton.addEventListener('click', e => {
+      mimicServerCall()
+      .then(() => {
+        if (likeButton.textContent !== FULL_HEART){
+          likeButton.textContent = FULL_HEART
+          likeButton.classList.add('activated-heart')
+        }else{
+          likeButton.classList.remove('activated-heart')
+          likeButton.textContent = EMPTY_HEART
+        }     
+      })
+      .catch((errorMessage) => {
+        document.querySelector('div#modal').className=''
+        document.querySelector('div#modal p').textContent = errorMessage
+        setTimeout(() => {
+          document.querySelector('div#modal').className = "hidden"
+        }, 3000);
+      })
+    })
+})
 
 
 //------------------------------------------------------------------------------
